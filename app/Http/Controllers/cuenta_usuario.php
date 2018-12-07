8e75b824cbc128436ab74b5c87487b5c607c02d6
al221610794@gmail.com <lfgbing@gmail.com>
2018-12-06 23:24:17 -0600

    login cuenta_usuario

--- app/Http/Controllers/cuenta_usuario.php
+++ app/Http/Controllers/cuenta_usuario.php
@@ -11,13 +11,30 @@ use App\pacientes;
 class cuenta_usuario extends Controller
 {
     public function consultar_perfil(){
-		$usu=pacientes::withTrashed()->where('id_pac','=',Session::get('sesionid'))->get();
-		if(count($usu)==0){
-			$proceso="Consultar Perfil del Usuario";
-			$mensaje="No existe registro alguno del usuario.";
-			return view('sistema.mensaje')->with('proceso',$proceso)->with('mensaje',$mensaje);
+		if(Session::get('sesiontipo')=="usu"){
+			if(Session::get('sesionid')==""){
+				return redirect()->route('logindesact');
+			}else{
+				//return redirect()->route('loginnoruta');
+				$usu=pacientes::withTrashed()->where('id_pac','=',Session::get('sesionid'))->get();
+				if(count($usu)==0){
+					$proceso="Consultar Perfil del Usuario";
+					$mensaje="No existe registro alguno del usuario.";
+					return view('sistema.mensaje')->with('proceso',$proceso)->with('mensaje',$mensaje);
+				}else{
+					return view('sistema.perfil_usuario')->with('usuario',$usu);
+				}
+			}
 		}else{
-			return view('sistema.perfil_usuario')->with('usuario',$usu);
+			if(Session::get('sesiontipo')=="admin"){
+				if(Session::get('sesionid')==""){
+					return redirect()->route('logindesact');
+				}else{
+					return redirect()->route('loginnoruta');
+				}
+			}else{
+				return redirect()->route('loginempty');
+			}
 		}
 	}
 }
