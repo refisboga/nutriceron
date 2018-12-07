515f80e9347987c0fcfc0fe3f66a37ca04137765
al221610794@gmail.com <lfgbing@gmail.com>
2018-12-06 23:32:17 -0600

    login expediente

--- app/Http/Controllers/expediente.php
+++ app/Http/Controllers/expediente.php
@@ -12,13 +12,30 @@ class expediente extends Controller
 {
 	
     public function consultar_expediente(){
-		$e=expedientes::withTrashed()->where('id_pac_fk','=',Session::get('sesionid'))->get();
-		if(count($e)==0){
-			$proceso="Consultar Expedientes";
-			$mensaje="Necesitas hacer una Pre-Evaluacion, para tener un Expediente.";
-			return view('sistema.mensaje')->with('proceso',$proceso)->with('mensaje',$mensaje);
+		if(Session::get('sesiontipo')=="usu"){
+			if(Session::get('sesionid')==""){
+				return redirect()->route('logindesact');
+			}else{
+				//return redirect()->route('loginnoruta');
+				$e=expedientes::withTrashed()->where('id_pac_fk','=',Session::get('sesionid'))->get();
+				if(count($e)==0){
+					$proceso="Consultar Expedientes";
+					$mensaje="Necesitas hacer una Pre-Evaluacion, para tener un Expediente.";
+					return view('sistema.mensaje')->with('proceso',$proceso)->with('mensaje',$mensaje);
+				}else{
+					return view('sistema.expediente')->with('expe',$e);
+				}
+			}
 		}else{
-			return view('sistema.expediente')->with('expe',$e);
+			if(Session::get('sesiontipo')=="admin"){
+				if(Session::get('sesionid')==""){
+					return redirect()->route('logindesact');
+				}else{
+					return redirect()->route('loginnoruta');
+				}
+			}else{
+				return redirect()->route('loginempty');
+			}
 		}
 	}
 }
